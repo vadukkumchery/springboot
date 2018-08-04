129404da1645141fb463f847c5386777392f1aec
Bidhudas <bidhu@bidhu.co>
2018-08-05 00:04:36 +0200

    Sample Sprintboot App

---
+++ spring-boot/src/main/java/co/bidhu/springboot/SpringBootApiApp.java
@@ -0,0 +1,16 @@
+package co.bidhu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(SpringBootApiApp.class, args);
+		
+	}
+
+}
