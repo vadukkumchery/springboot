e150f07c1885be4f82a7ae34644fefc6ca2d048b
Bidhudas <bidhudas@gmail.com>
2018-08-08 14:23:17 +0200

    webhook testing

--- spring-boot/src/main/java/co/bidhu/springboot/topic/TopicService.java
+++ spring-boot/src/main/java/co/bidhu/springboot/topic/TopicService.java
@@ -12,7 +12,7 @@ public class TopicService {
 	List<Topic> topics = new ArrayList <> (Arrays.asList(new Topic("spring","Spring Frame Wokr", "Spring Framework Description"),
 			new Topic("java","Core Java", "Core Java Description"),
 			new Topic("js","Node JS", "NodeJs Description"),
-			new Topic("aws","Amazon Web Service", "Amazon Web Services lambda")));
+			new Topic("aws","Amazon Web Service", "Amazon Web Services Architect")));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
