c40a43bcc80597ebb82d7eb485bdd871a144e906
Bidhudas <bidhudas@gmail.com>
2018-08-09 16:24:30 +0200

    webservice call to webservice

--- spring-boot/src/main/resources/static/js/springboot.js
+++ spring-boot/src/main/resources/static/js/springboot.js
@@ -1,10 +1,10 @@
-var app = angular.module("springModule", []).controller("springController", function($scope) {
+var app = angular.module("springModule", []).controller("springController", function($scope,$http) {
 	
-	 var topics =[
-		 	{id:"spring",name:"Spring Frame Wokr", description:"Spring Framework Description"},
-			{id:"java",name:"Core Java", description:"Core Java Description"},
-			{id:"js",name:"Node JS", description:"NodeJs Description"},
-			{id:"aws",name:"Amazon Web Service", description:"Amazon Web Services Architect"}
-	 ];
-	 $scope.topics = topics;
+	$http.get('http://localhost/topics').then(function(response) {
+		var topics = response.data
+		$scope.topics = topics;
+	});
 });
+	
+	
+	 
