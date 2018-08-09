8103d77a4f22d28efa1a61e5d663862ac0fa2b88
Bidhudas <bidhudas@gmail.com>
2018-08-09 20:35:32 +0200

    to avoid CORS, used the URL

--- spring-boot/src/main/resources/static/js/springboot.js
+++ spring-boot/src/main/resources/static/js/springboot.js
@@ -1,8 +1,12 @@
 var app = angular.module("springModule", []).controller("springController", function($scope,$http) {
 	
-	$http.get('http://localhost/topics').then(function(response) {
+	$http.get('http://springbootapp-env.2u4jsb3uad.eu-west-3.elasticbeanstalk.com/topics').then(function(response) {
+			
+		
 		var topics = response.data;
 		$scope.topics = topics;
+	}).catch(function(reason) {
+		
 	});
 });
 	
