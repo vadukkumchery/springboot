5cbce9b1cf93ecfdc1bf8df7475c43cf971c82b7
Bidhudas <bidhudas@gmail.com>
2018-08-09 20:52:14 +0200

    cross origin anotation added

--- spring-boot/src/main/java/co/bidhu/springboot/topic/TopicController.java
+++ spring-boot/src/main/java/co/bidhu/springboot/topic/TopicController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class TopicController {
 	private TopicService topicService;
 
 	@RequestMapping("/topics")
+	@CrossOrigin
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
