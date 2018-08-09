ca250db68133da44c2f61e104f1380080c4a0741
Bidhudas <bidhudas@gmail.com>
2018-08-09 20:53:09 +0200

    changed to localhost url

--- spring-boot/src/main/resources/static/js/springboot.js
+++ spring-boot/src/main/resources/static/js/springboot.js
@@ -1,6 +1,6 @@
 var app = angular.module("springModule", []).controller("springController", function($scope,$http) {
 	
-	$http.get('http://springbootapp-env.2u4jsb3uad.eu-west-3.elasticbeanstalk.com/topics').then(function(response) {
+	$http.get('http://localhost/topics').then(function(response) {
 			
 		
 		var topics = response.data;
